60884f635706fd9beb2759a1b72c946150ddbd9e
ririErliza <ryantiadam@yahoo.co.uk>
2022-03-18 14:36:04 +0100

    fetch data, hide button, same height cards
    same height cards : add ellipsis to the text title (by adding class truncate-text), set the style of img (width, height, object-fit)  and add bootstrap class h-100 to each card

---
+++ index.js
@@ -0,0 +1,110 @@
+/* Load books function */
+
+// const loadBooks = () => {
+//     const url = "https://striveschool-api.herokuapp.com/books"
+
+// fetch (url)
+// .then(response => response.json() )
+// .then(data => {
+//   console.log(data)
+    
+//     let cardsWrapper = document.querySelector ("#cardsWrapper")
+
+ 
+//     cardsWrapper.innerHTML =""
+//     data.forEach(element => {
+//         const content=
+//         `<div class="col-md-4 hideThis">
+//         <div class="card mb-4 shadow-sm">
+//                 <img src=${element.img} class="card-image img-fluid" />
+//                 <div class="card-body">
+//                   <p class="card-text">
+//                     This is an image.
+//                   </p>
+//                   <div class="d-flex justify-content-between align-items-center">
+//                     <div class="btn-group">
+//                       <button type="button" class="btn btn-sm btn-outline-secondary">
+//                         View
+//                       </button>
+//                       <button type="button" class="btn btn-sm btn-outline-secondary " onclick="hideFunction()">
+//                         Hide
+//                       </button>
+//                     </div>
+//                     <small class="text-muted">${element.title}</small>
+//                   </div>
+//                 </div>
+//               </div>
+//             </div>`
+
+//             cardsWrapper.innerHTML += content
+//     })
+// })
+// .catch(error => {
+//     console.log(error)
+// })
+// }
+
+
+const cardsWrapper = document.querySelector("#cardsWrapper")
+
+    const loadBooks = () => {
+
+      const url = "https://striveschool-api.herokuapp.com/books"
+
+      fetch(url)
+      .then(response => response.json())
+      .then(booksArray => {
+        console.log(booksArray)
+        // DOM MANIPULATION WITH MAP
+
+        cardsWrapper.innerHTML = booksArray.map(book => `
+
+        <div class="col-6 col-md-3 mb-5 hideThis">
+                    <div class="card h-100">
+                            <img src="${book.img}" class="card-img-top" alt="HTML">
+                            <div class="card-body">
+                                <h6 class="card-title text-truncate"> ${book.title} </h6>
+                                <p class="card-text">${book.category}</p>
+                                
+
+                                <div>
+                                    <span class="font-weight-bold">$ ${book.price}</span>  
+                                </div>
+                            </div>
+
+                            <div class="card-footer d-flex flex-row justify-content-between bg-light">
+                                <button class="mr-3 btn btn-sm border border-secondary text-muted" onclick="hideFunction()">
+                                    Hide
+                                </button>
+                    
+                                <button class="mr-3 btn btn-sm">
+                                <i class="bi bi-plus-square-fill"></i>
+                                </button>
+                            </div>
+
+                    </div>
+                </div>`).join("")
+
+
+
+
+
+        })
+    }
+
+    /* hide function */
+
+function hideFunction() {
+    let x = document.querySelector(".hideThis");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+
+
+          window.onload = () => {
+            loadBooks();
+          }
